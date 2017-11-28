42e4d508d1f5275570cbd35fed28421480cf0467
root <root@fcrn-ubuntu16.ouce.ox.ac.uk>
2017-11-28 11:01:24 +0000

    Up to date changes 28 Nov

--- sites/all/themes/foodbytes/gulpfile.js
+++ sites/all/themes/foodbytes/gulpfile.js
@@ -6,8 +6,9 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var importer = require('node-sass-globbing');
 var plumber = require('gulp-plumber');
-var cssmin = require('gulp-cssmin');
+var bulkSass = require('gulp-sass-glob-import');
 var browserSync = require('browser-sync').create();
+
 
 var sass_config = {
   importer: importer,
@@ -17,26 +18,34 @@ var sass_config = {
     'node_modules/compass-mixins/lib/'
   ]
 };
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        //injectChanges: true,
-        proxy: "foodsource.org.uk:8888"
-    });
-    gulp.watch("./sass/**/*.scss", ['sass']).on('change', browserSync.reload);
-});
-
+ 
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
     .pipe(plumber())
-    //.pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
+    .pipe(bulkSass ({ extensions: ['.scss'] }))
     .pipe(sass(sass_config).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 version']
     }))
-    //.pipe(sourcemaps.write('.'))
-    .pipe(cssmin())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('default', [ 'browser-sync']);
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        injectChanges: true,
+        proxy: "127.0.0.1/foodsource"
+    });
+    gulp.watch("./sass/**/*.scss", ['sass']);
+    gulp.watch("./css/**/*.css").on('change', browserSync.reload);
+    gulp.watch("./js/**/*.js", ['uglify']).on('change', browserSync.reload);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('sass/**/*.scss', ['sass']);
+}); 
+
+gulp.task('default', ['watch']);
+
